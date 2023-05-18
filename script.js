03d627b7f646b427007a7b19e37479e05adc3f9c
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2023-05-18 16:59:43 -0300

    added checkbox js verification

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('form')
+const checkboxes = document.querySelectorAll('input[type=checkbox]')
 const username = document.getElementById('username')
 const email = document.getElementById('email')
 const message = document.getElementById('message')
@@ -6,11 +7,15 @@ const message = document.getElementById('message')
 form.addEventListener('submit', (event) => {
     event.preventDefault()
 
-    // verificar se um dos botões está selecionado
+    if (!areCheckboxesSelected(checkboxes)) {
+        // colocar a classe error no input
+        checkbox.classList.remove('checkbox-error');
+        return
+    }
 
     if (username.value === '' || !isUsernameValid(username.value)) {
         // colocar a classe error no input
-        console.log("erro no nome!");
+        username.classList.remove('username-error');
         return
     } 
 
@@ -29,15 +34,26 @@ form.addEventListener('submit', (event) => {
     form.onsubmit();
 })
 
+function areCheckboxesSelected(checkboxes) {
+    const checkedCheckboxes = [];
+    
+    for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            checkedCheckboxes.push(checkboxes[i]);
+        }
+    }
+
+    return checkedCheckboxes.length > 0;
+}
+
 function isUsernameValid(username) {
     const words = username.split(" ");
     return words.length >= 2;
-  }
+}
 
 function isEmailValid(email) {
     const emailRegex = new RegExp(
         /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
     )
-
     return emailRegex.test(email);
 }
